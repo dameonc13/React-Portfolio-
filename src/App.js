e656676170a2195fe6c90d8df819613e6fd7316e
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-04-21 18:27:01 -0400

    updated changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Home} />
+        <Route path="/" component={Home} />
       </div>
     </Router>
   );
