7eb4fc7572c116e4c6f8eedfd592c2b50ff6cf1a
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-04-21 19:26:41 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.7f23a8da4e4a39123f72165e3fe2b3eb.js"
+  "/React-Portfolio-/precache-manifest.a231389028d734f527240de666e7a683.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
