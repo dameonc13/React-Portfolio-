410522e36cc4e7a4f8bb5d41c178b2d902f80c9a
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-04-21 19:22:54 -0400

    Updates

---
+++ precache-manifest.7f23a8da4e4a39123f72165e3fe2b3eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea469fcd1f459bc7b6e372820de4f36c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "44affb7802a27f23ca69",
+    "url": "/React-Portfolio/static/css/main.4c68ea34.chunk.css"
+  },
+  {
+    "revision": "c2915a492cd95b782e96",
+    "url": "/React-Portfolio/static/js/2.84c6d73d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.84c6d73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44affb7802a27f23ca69",
+    "url": "/React-Portfolio/static/js/main.032211e1.chunk.js"
+  },
+  {
+    "revision": "910ea996016562bed44c",
+    "url": "/React-Portfolio/static/js/runtime-main.0af1ea3a.js"
+  },
+  {
+    "revision": "5e11a133edc5eb9fbe86f17c713bd6e6",
+    "url": "/React-Portfolio/static/media/bg.5e11a133.jpg"
+  },
+  {
+    "revision": "cd01f33de94c1c473621b5561e6c918d",
+    "url": "/React-Portfolio/static/media/logo-bg.cd01f33d.jpg"
+  }
+]);
