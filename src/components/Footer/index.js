b589b7942ba1a781e379acd61f5dbb5dccb899ad
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-06-02 09:54:55 -0400

    added icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,13 +9,40 @@ function Footer() {
           <footer>
             <section>
               {" "}
-              <i
-                className="fa fa-github"
-                //  style="font-size:48px;color:red"
-                onclick="window.location.href='https://github.com/dameonc13'"
-              ></i>
+              <a
+                href="https://github.com/dameonc13"
+                rel="noopener noreferrer"
+                target="__blank"
+              >
+                {" "}
+                <i
+                  className="fa fa-github"
+                  //  style="font-size:48px;color:red"
+                  onClick="window.location.href=''"
+                ></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/dameon-charley-0990518b/"
+                rel="noopener noreferrer"
+                target="__blank"
+              >
+                <i
+                  className="fa fa-linkedin"
+                  //  style="font-size:48px;color:red"
+                ></i>
+              </a>
+              <a
+                href="mailto:dameoncharley13@hotmail.com"
+                rel="noopener noreferrer"
+                target="__blank"
+              >
+                <i
+                  className="fa fa-envelope-square"
+                  //  style="font-size:48px;color:red"
+                ></i>
+              </a>
             </section>
-            <p>Github</p>
+
             <p>Copyright &copy;</p>
           </footer>
         </div>
